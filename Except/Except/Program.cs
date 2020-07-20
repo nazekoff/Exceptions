851cd1ccb494ba7255ffb78be80c726873f1db0f
postscript100@gmail.com <postscript100@gmail.com>
2020-07-20 15:36:50 +0600

    Заменено на !!!!!!!

--- Except/Except/Program.cs
+++ Except/Except/Program.cs
@@ -6,7 +6,7 @@ namespace Except
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("!!!!!!!!!!!");
         }
     }
 }
