d4568255dd7ba4ca8c5b6d4b6164d47f980f5d2f
postscript100@gmail.com <postscript100@gmail.com>
2020-07-20 15:35:29 +0600

    Создали проект Hello World

---
+++ Except/Except/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Except
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
