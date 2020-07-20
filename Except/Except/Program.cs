7883dea7944178e608650ab8f1d58bdf7951889c
postscript100@gmail.com <postscript100@gmail.com>
2020-07-20 15:43:01 +0600

    Урок по Исключениям

--- Except/Except/Program.cs
+++ Except/Except/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Except
 {
@@ -6,7 +7,33 @@ namespace Except
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("!!!!!!!!!!!");
+            var r = GetRowCount("C:/Path/file.txt");
+            Console.WriteLine(r);
+        }
+
+        static int GetRowCount(string path)
+        {
+            try
+            {
+                var rows = File.ReadAllLines(path);
+                return rows.Length;
+            }
+            catch (FileNotFoundException ex)
+            {
+                //Console.WriteLine($"Файл {path} не был найден. Alarm!");
+                Console.WriteLine(ex.Message);
+                return 0;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Console.WriteLine("Функция была вызвана с пустым аргументом. Так делать не хорошо.");
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Не получилось считать файл");
+                return 0;
+            }
         }
     }
 }
